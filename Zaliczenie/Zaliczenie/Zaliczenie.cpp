9127c37a89d292a29bc53250c6b73b3222dd0c9c
Rafał Wątroba <w65575@student.wsiz.edu.pl>
2022-06-30 10:22:09 +0200

    First Comit

---
+++ Zaliczenie/Zaliczenie/Zaliczenie.cpp
@@ -0,0 +1,10 @@
+﻿// Zaliczenie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
